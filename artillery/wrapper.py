3d7d88498e3c110bdb182615942f22a9455355c3
zanderhavgaard <zander@havgaard.dk>
2022-05-10 14:34:28 +0200

    implemented POC of wrapping artillery cli in python

--- artillery/wrapper.py
+++ artillery/wrapper.py
@@ -1,22 +1,119 @@
 import json
-
-json_file = "result.txt"
-output_json_file = "result.json"
-
-with open(json_file, "r") as _file:
-    # read all lines in file into a list
-    lines = _file.readlines()
-    # new list for parsed JSON objects
-    json_lines = []
-    # create a list of python dicts that we can then drop as a single JSON structure
-    for line in lines:
-        # get rid of newlines in the output ...
-        if line == "\n":
-            continue
-        json_lines.append(json.loads(json.loads(line)["body"]))
-
-# write json output to a new file
-with open(output_json_file, "w+") as _file:
-    _file.write(json.dumps(json_lines, indent=4))
+from subprocess import CompletedProcess
+from shell import shell
 
 
+def parse_result_txt_json_file(filename: str) -> list:
+    # open file as read only
+    with open(filename, "r") as _file:
+        # read all lines in file into a list
+        lines = _file.readlines()
+        # new list for parsed JSON objects
+        json_lines = []
+        # create a list of python dicts that we can then drop as a single JSON structure
+        for line in lines:
+            # get rid of newlines in the output ...
+            if line == "\n":
+                continue
+            json_lines.append(json.loads(json.loads(line)["body"]))
+
+    return json_lines
+
+
+def write_parsed_json_to_file(json_list: list, output_filename: str) -> None:
+    # write json output to a new file, allow to create the file if not exists
+    # will overwrite existing file with that filename
+    with open(output_filename, "w+") as _file:
+        _file.write(json.dumps(json_list, indent=4))
+
+
+def write_subprocess_stdout_to_file(proc: CompletedProcess, filename: str) -> None:
+    """Write stdout of a completed process to a file"""
+    with open(filename, "w+") as _file:
+        _file.write(proc.stdout)
+
+
+def run_artillery_script(script_path: str, report_path: str, stdout_filename: str = None) -> None:
+    """
+    script_path: path to artillery .yaml script file to run
+    report_path: path to output artillery report .json file
+    stdout_filename: optional path to write stdout to specified filename
+        this is useful for saving response bodies logged to stdout
+
+    will run artillery using the cli as a subprcess
+    """
+
+    # build the shell command to run
+    artillery_cmd = f"artillery run --quiet {script_path} --output {report_path}"
+
+    print("Running artillery cli using the following shell command: ")
+    print(artillery_cmd)
+    print("Starting artillery ...")
+
+    # create the shell process
+    # the program will wait here for the shell subprocess to finish
+    art_proc = shell(artillery_cmd)
+
+    # check if artillery exited succesfully
+    if art_proc.returncode == 0:
+        print("Done running artillery")
+        if stdout_filename:
+            write_subprocess_stdout_to_file(art_proc, stdout_filename)
+    else:
+        print("ERROR: something went wrong running artillery, dumping stdout and stderr")
+        print(f"artillery return code: {art_proc.returncode}")
+        print("stdout")
+        print(art_proc.stdout)
+        print("stderr")
+        print(art_proc.stderr)
+        print("--- end of artillery error output ---")
+
+
+def generate_artillery_report(input_file: str, output_file: str) -> None:
+    """
+    input_file: artillery .json report file
+    output_file: filename for the .html report file to generate
+
+    Uses the artillery cli to generate a html report from a completed artillery .json report
+    """
+    generate_report_cmd = f"artillery report {input_file} --output {output_file}"
+
+    print("Running artillery report with the following shell command:")
+    print(generate_report_cmd)
+    print("Generating artillery report ...")
+
+    generate_proc = shell(generate_report_cmd)
+
+    if generate_proc.returncode == 0:
+        print("Successfully generated report.")
+    else:
+        print("ERROR: artillery report did not exit successfully.")
+        print(f"artillery return code: {generate_report_cmd.returncode}")
+        print("stdout")
+        print(generate_report_cmd.stdout)
+        print("stderr")
+        print(generate_report_cmd.stderr)
+        print("--- end of artillery error output ---")
+
+
+if __name__ == "__main__":
+    """
+    example useage.
+
+    this code is only executed when invoking this file directly, eg:$ python wrapper.py
+    """
+    artillery_script = "benchmark.yaml"
+    report_filename = "report.json"
+    json_file = "result.txt"
+    output_json_file = "result.json"
+    html_report_filename = "report.html"
+
+    # run the artillery script, will produce the report.json and all json bodies as result.txt
+    run_artillery_script(artillery_script, report_filename, json_file)
+
+    # parse result.txt to a valid JSON list and save to result.json
+    parsed_json = parse_result_txt_json_file(json_file)
+    write_parsed_json_to_file(parsed_json, output_json_file)
+
+    # parse the generated report.json to a visual html report, and save to a file
+    generate_artillery_report(report_filename, html_report_filename)
